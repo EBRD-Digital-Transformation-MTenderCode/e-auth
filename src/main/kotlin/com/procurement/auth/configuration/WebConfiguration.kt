927751dd3bd0cde5f3708ed30abea36c96d3c333
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-02-09 16:45:33 +0200

    Appended filter for MDC.

--- src/main/kotlin/com/procurement/auth/configuration/WebConfiguration.kt
+++ src/main/kotlin/com/procurement/auth/configuration/WebConfiguration.kt
@@ -1,10 +1,12 @@
 package com.procurement.auth.configuration
 
+import org.springframework.boot.web.servlet.ServletComponentScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
 @EnableWebMvc
+@ServletComponentScan(basePackages = ["com.procurement.auth.filter"])
 @ComponentScan(basePackages = ["com.procurement.auth.controller"])
 class WebConfiguration
